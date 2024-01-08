9db4fd2463fb6436c770f1b409fd1e45f3f3baea
Guilherme Macedo <130194662+Yguilhermemacedo@users.noreply.github.com>
2024-01-08 19:15:31 -0300

    portifolio

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
+ document.addEventListener('DOMContentLoaded', function () {
+     const toggleBtn = document.querySelector('.toggle-btn');
+     const menu = document.querySelector('.menu');
+     const fecharMenuBtn = document.querySelector('.fechar-menu');
+
+     toggleBtn.addEventListener('click', function () {
+         menu.classList.toggle('open');
+     });
+
+     fecharMenuBtn.addEventListener('click', function () {
+         menu.classList.remove('open');
+     });
+ });
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    // Configuração do atraso inicial em milissegundos
+    
     let atrasoInicial = 1000;
 
-    // Obtém o elemento com a classe 'texto-escrito'
+    
     let elementoTexto = document.querySelector('.tittle-home');
 
-    // Obtém o texto dentro do elemento
+    
     let texto = elementoTexto.textContent;
 
-    // Limpa o conteúdo do elemento
+    
     elementoTexto.textContent = '';
 
-    // Função para adicionar cada letra com um atraso
+    
     function adicionarLetras(index) {
         if (index < texto.length) {
             elementoTexto.textContent += texto.charAt(index);
             setTimeout(function() {
                 adicionarLetras(index + 1);
-            }, 100); // Ajuste o valor 100 para controlar a velocidade de digitação
+            }, 100); 
         }
     }
 
-    // Inicia a animação após o atraso inicial
     setTimeout(function() {
         adicionarLetras(0);
     }, atrasoInicial);
